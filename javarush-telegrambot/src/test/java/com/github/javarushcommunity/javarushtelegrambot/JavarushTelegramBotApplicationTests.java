a0143626d6a1701a3da5f1bf869fc07e10add167
neocod2020 <neocod2020@gmail.com>
2023-05-01 20:52:55 +0300

    Initial commit. Spring Boot basis.

---
+++ javarush-telegrambot/src/test/java/com/github/javarushcommunity/javarushtelegrambot/JavarushTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.javarushcommunity.javarushtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarushTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
